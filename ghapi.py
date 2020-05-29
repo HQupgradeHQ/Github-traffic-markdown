86abeab1d5d224af7e87d536fb6a1f2619a77233
FooBar <18361586+HQupgradeHQ@users.noreply.github.com>
2020-05-29 07:02:00 +0300

    new traffic.md generation

--- ghapi.py
+++ ghapi.py
@@ -1,134 +1,152 @@
-import requests
-import concurrent.futures
+from dataclasses import dataclass
 from collections import defaultdict
 from subprocess import run
+import requests
+import concurrent.futures
 
 
-def paths_base_url(user, repo):
-    return f"https://api.github.com/repos/{user}/{repo}/traffic/popular/paths"
+@dataclass
+class GithubRepo:
+    username: str
+    repo: str
 
+    @property
+    def paths(self):
+        return f"https://api.github.com/repos/{self.username}/{self.repo}/traffic/popular/paths"
 
-def views_base_url(user, repo):
-    return f"https://api.github.com/repos/{user}/{repo}/traffic/views"
+    @property
+    def views(self):
+        return f"https://api.github.com/repos/{self.username}/{self.repo}/traffic/views"
 
-
-def clones_base_url(user, repo):
-    return f"https://api.github.com/repos/{user}/{repo}/traffic/clones"
-
-
-def extract_base_url(url):
-    "extract repo name from *_base_url"
-    t = url.rfind("traffic") - 1
-    r = url.rfind("/", 0, t) + 1
-    return url[r:t]
-
-
-def load_url(url):
-    r = requests.get(url, headers=headers)
-    return r.json()
-
-
-def clean_paths(data):
-    result = "**paths** :open_file_folder:<br/>\n"
-    for p in data:
-        path = p["path"]
-        result += (
-            f"`github.com{path}`: all = {p['count']}, unique = {p['uniques']}<br/>\n"
+    @property
+    def clones(self):
+        return (
+            f"https://api.github.com/repos/{self.username}/{self.repo}/traffic/clones"
         )
-    return result
+
+    @staticmethod
+    def extract_base_url(url):
+        "extract repo name from *_base_url"
+        t = url.rfind("traffic") - 1
+        r = url.rfind("/", 0, t) + 1
+        return url[r:t]
 
 
-def clean_clones(data):
-    result = ""
-    if data["count"]:
-        result += "**clones** :octocat: :octocat:<br/>\n"
+@dataclass
+class GithubMarkdown:
+    data: defaultdict
+
+    def generate_paths_block(self, paths):
+        if paths is None:
+            return ""
+        result = "**paths** <br/>\n"
+        for p in paths:
+            path = p["path"]
+            result += f"`github.com{path}`: all = {p['count']}, unique = {p['uniques']}<br/>\n"
+        return result
+
+    def generate_clones_block(self, clones):
+        if clones is None:
+            return ""
+        result = "**clones** <br/>\n"
         result += "| Day | all | unique |\n"
         result += "| --- | :---: | :---: |\n"
-        for cl in data["clones"]:
+        for cl in clones["clones"]:
             result += f"| `{cl['timestamp'][:10]}` | {cl['count']} |{cl['uniques']} |\n"
-        result += f"| `total` | {data['count']} | {data['uniques']} |\n"
+        result += f"| `total` | {clones['count']} | {clones['uniques']} |\n"
         result += "\n"
 
-    return result
+        return result
 
-
-def clean_views(data):
-    result = ""
-    if data["count"]:
-        result = "**views** :eyes:<br/>\n"
+    def generate_views_block(self, views):
+        if views is None:
+            return ""
+        result = "**views** <br/>\n"
         result += "| Day | all | unique |\n"
         result += "| --- | :---: | :---: |\n"
-        for v in data["views"]:
+        for v in views["views"]:
             result += f"| `{v['timestamp'][:10]}` | {v['count']} |{v['uniques']} |\n"
-        result += f"| `total` | {data['count']} | {data['uniques']} |\n"
+        result += f"| `total` | {views['count']} | {views['uniques']} |\n"
         result += "\n"
 
-    return result
+        return result
+
+    def generate_page(self, order=[]):
+        data = self.data.items()
+        # rearrange order as it in target list https://stackoverflow.com/a/52044835
+        if order:
+            data = {k: self.data[k] for k in order}
+            data = data.items()
+        md_content = ""
+        for repo, traffic in data:
+            md_content += f"# {repo} <br/>\n" if traffic else ""
+            views = self.generate_views_block(traffic.get("views", None))
+            paths = self.generate_paths_block(traffic.get("paths", None))
+            clones = self.generate_clones_block(traffic.get("clones", None))
+
+            md_content += "".join([views, paths, clones])
+        with open("traffic.md", "w") as f:
+            f.write(md_content)
+
+
+@dataclass
+class Loader:
+    api_key: str
+    username: str
+    repo_list: list
+
+    def load_url(self, url):
+        r = requests.get(url, headers={"Authorization": "token " + self.api_key})
+        return r.json()
+
+    def generate_urls(self):
+        urls = []
+        for r in self.repo_list:
+            urls.append(GithubRepo(self.username, r).views)
+            urls.append(GithubRepo(self.username, r).paths)
+            urls.append(GithubRepo(self.username, r).clones)
+        return urls
+
+    def dispatch(self):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            # Start the load operations and mark each future with its URL
+            future_to_url = {
+                executor.submit(self.load_url, url): url for url in self.generate_urls()
+            }
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    data = future.result()
+                except Exception as exc:
+                    print("%r generated an exception: %s" % (url, exc))
+                else:
+                    update_dict(url, data)
 
 
 def update_dict(url, data):
     """populate dict, check if there is data"""
-    current_repo = extract_base_url(url)
+    current_repo = GithubRepo.extract_base_url(url)
     if "paths" in url and data:
-        checked_paths = clean_paths(data)
-        if checked_paths:
-            u_dict[current_repo]["paths"] = checked_paths
-    if "clones" in url and data:
-        checked_clones = clean_clones(data)
-        if checked_clones:
-            u_dict[current_repo]["clones"] = checked_clones
-    if "views" in url and data:
-        checked_views = clean_views(data)
-        if checked_views:
-            u_dict[current_repo]["views"] = checked_views
+        raw_dict[current_repo]["paths"] = data
+    if "clones" in url and data["count"]:
+        raw_dict[current_repo]["clones"] = data
+    if "views" in url and data["count"]:
+        raw_dict[current_repo]["views"] = data
 
 
-def get_stats(urls):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        # Start the load operations and mark each future with its URL
-        future_to_url = {executor.submit(load_url, url): url for url in urls}
-        for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                print("%r generated an exception: %s" % (url, exc))
-            else:
-                update_dict(url, data)
-
-
-def generate_md(u_dict):
-    md_content = ""
-    for repo, traffic in u_dict.items():
-        md_content += f"# {repo} <br/>\n"
-        for _, part in traffic.items():
-            md_content += part
-    return md_content
-
-
-token = ""  # with repo scope
-headers = {"Authorization": "token " + token}
-u = ""  # username
-repos_u = []  # repos
-# d = "" # also works with organization
-# repos_d = [] # org's repos
-u_dict = defaultdict(dict)  # everything from get_stats will appear here
-u_urls = []
-for repo in repos_u:
-    u_urls.append(paths_base_url(u, repo))
-    u_urls.append(views_base_url(u, repo))
-    u_urls.append(clones_base_url(u, repo))
-# d_urls = []
-# for repo in repos_d:
-#    d_urls.append(paths_base_url(d, repo))
-#    d_urls.append(views_base_url(d, repo))
-#    d_urls.append(clones_base_url(d, repo))
-# get_stats(urls=d_urls)
-get_stats(urls=u_urls)
-
-with open("traffic.md", "w") as f:
-    f.write(generate_md(u_dict))
+raw_dict = defaultdict(dict)
+token = "insert token here "  # with repo scope
+u = "upgradeQ"
+repos_u = [
+    "osuplaylist",
+    "Obscounter",
+    "Daylight",
+    "Awesome-keyboard-typing",
+    "Github-traffic-markdown",
+]
+Loader(api_key=token, username=u, repo_list=repos_u).dispatch()
+GithubMarkdown(raw_dict).generate_page(order=repos_u)
 try:
-    run("grip traffic.md -b") # open browser
+    run("grip traffic.md -b")
 except KeyboardInterrupt:
     pass
--- traffic.md
+++ traffic.md
@@ -1,45 +1,55 @@
-# dotafiles <br/>
-**clones** :octocat: :octocat:<br/>
-| Day | all | unique |
-| --- | :---: | :---: |
-| `2020-05-07` | 3 |3 |
-| `2020-05-08` | 1 |1 |
-| `total` | 4 | 4 |
-
-# Daylight <br/>
-**views** :eyes:<br/>
-| Day | all | unique |
-| --- | :---: | :---: |
-| `2020-05-03` | 1 |1 |
-| `2020-05-05` | 1 |1 |
-| `2020-05-07` | 2 |1 |
-| `2020-05-08` | 3 |1 |
-| `total` | 7 | 4 |
-
-**paths** :open_file_folder:<br/>
-`github.com/HQupgradeHQ/Daylight`: all = 5, unique = 4<br/>
-`github.com/HQupgradeHQ/Daylight/blob/master/osu_overlay.py`: all = 2, unique = 2<br/>
-# Awesome-keyboard-typing <br/>
-**paths** :open_file_folder:<br/>
-`github.com/HQupgradeHQ/Awesome-keyboard-typing`: all = 4, unique = 4<br/>
-**clones** :octocat: :octocat:<br/>
-| Day | all | unique |
-| --- | :---: | :---: |
-| `2020-05-10` | 4 |3 |
-| `total` | 4 | 3 |
-
-**views** :eyes:<br/>
-| Day | all | unique |
-| --- | :---: | :---: |
-| `2020-04-30` | 1 |1 |
-| `2020-05-02` | 2 |2 |
-| `2020-05-04` | 1 |1 |
-| `total` | 4 | 4 |
-
 # osuplaylist <br/>
-**clones** :octocat: :octocat:<br/>
+**views** <br/>
 | Day | all | unique |
 | --- | :---: | :---: |
-| `2020-05-05` | 1 |1 |
+| `2020-05-24` | 3 |2 |
+| `total` | 3 | 2 |
+
+**paths** <br/>
+`github.com/HQupgradeHQ/osuplaylist`: all = 1, unique = 1<br/>
+`github.com/HQupgradeHQ/osuplaylist/releases`: all = 1, unique = 1<br/>
+`github.com/HQupgradeHQ/osuplaylist/tree/master/osuplaylist`: all = 1, unique = 1<br/>
+**clones** <br/>
+| Day | all | unique |
+| --- | :---: | :---: |
+| `2020-05-19` | 1 |1 |
 | `total` | 1 | 1 |
 
+# Obscounter <br/>
+**views** <br/>
+| Day | all | unique |
+| --- | :---: | :---: |
+| `2020-05-19` | 2 |1 |
+| `total` | 2 | 1 |
+
+**paths** <br/>
+`github.com/HQupgradeHQ/Obscounter`: all = 1, unique = 1<br/>
+`github.com/HQupgradeHQ/Obscounter/blob/master/hotkey_counter.py`: all = 1, unique = 1<br/>
+**clones** <br/>
+| Day | all | unique |
+| --- | :---: | :---: |
+| `2020-05-17` | 5 |5 |
+| `total` | 5 | 5 |
+
+# Daylight <br/>
+**views** <br/>
+| Day | all | unique |
+| --- | :---: | :---: |
+| `2020-05-20` | 6 |2 |
+| `total` | 6 | 2 |
+
+**paths** <br/>
+`github.com/HQupgradeHQ/Daylight`: all = 3, unique = 1<br/>
+`github.com/HQupgradeHQ/Daylight/blob/master/osu_overlay.py`: all = 1, unique = 1<br/>
+`github.com/HQupgradeHQ/Daylight/blob/master/pygame_overlay.py`: all = 1, unique = 1<br/>
+`github.com/HQupgradeHQ/Daylight/blob/master/readme.md`: all = 1, unique = 1<br/>
+# Awesome-keyboard-typing <br/>
+**views** <br/>
+| Day | all | unique |
+| --- | :---: | :---: |
+| `2020-05-25` | 1 |1 |
+| `2020-05-27` | 4 |2 |
+| `total` | 5 | 3 |
+
+**paths** <br/>
+`github.com/HQupgradeHQ/Awesome-keyboard-typing`: all = 5, unique = 3<br/>
